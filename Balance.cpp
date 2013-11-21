9325552cb94c99066515a7ad1b6339bc16a8e9b2
Christopher J. Cunniff <ccunniff@gmail.com>
2013-11-21 13:01:26 -0500

    initial Balance class
    keeps track of players credit throughout games

---
+++ Balance.cpp
@@ -0,0 +1,17 @@
+#include "Balance.h"
+
+Balance::Balance(void) {};
+
+Balance::~Balance(void) {};
+
+int Balance::addBalance(int amt) {
+	balance += amt;
+	return balance;
+};
+
+int Balance::subBalance(int amt) {
+	balance -= amt;
+	return balance;
+};
+
+int Balance::getBalance() {return balance;};
---
+++ Balance.h
@@ -0,0 +1,15 @@
+#ifndef BALANCE_H_
+#define BALANCE_H_
+
+class Balance {
+private:
+	static int balance; //amount of money the player has
+public:
+	Balance(void);
+	~Balance(void);
+	int	addBalance(int);
+	int	subBalance(int);
+	int getBalance(void);
+};
+
+#endif /*BALANCE_H_*/
