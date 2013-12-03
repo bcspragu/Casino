21404ff3bb0e4b94b15c1b38379889128e1b5605
Christopher J. Cunniff <ccunniff@gmail.com>
2013-12-03 10:40:18 -0500

    fixed random line retrieval

--- Advertisement.cpp
+++ Advertisement.cpp
@@ -2,28 +2,34 @@
 
 Advertisement::Advertisement(void) {
 	path = "advertisement.txt";
+    srand(time(NULL));
 }
 
 Advertisement::~Advertisement(void) {};
 
 string Advertisement::getAd(void) {
 	int numLines = 0;
-    string line;
+    string randLine;
     ifstream file(path.c_str());
 
-    //count number of lines
-    while (getline(file, line))
-        ++numLines;
+    if(file.is_open()) {
+        //count number of lines
+        while (getline(file, randLine))
+            numLines++;
     
-    //go back to begining of file
-    file.clear();
-    file.seekg(0, ios::beg);
-    
-    string temp;
-    for (int i = 0; i < (rand() % (numLines + 1)); i++) {
-    	getline(file, line);
-    }
-    file.close();
-    
-	return line;
+        //go back to begining of file
+        file.clear();
+        file.seekg(0, ios::beg);
+
+        //select a random line from file
+        string temp;
+        
+        int randNum = rand();
+        for (int i = 0; i < randNum % numLines + 1; i++) {
+        	getline(file, randLine);
+        }
+        file.close();
+    } else 
+        return "Error in retrieving advertisement";
+	return randLine;
 }
